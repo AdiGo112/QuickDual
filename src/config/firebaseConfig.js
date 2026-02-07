a6f99e6824ee6a969cbbe3a0a84ab8b8f1e85fc2
Aditya Gorane <168202112+AdiGo112@users.noreply.github.com>
2026-02-08 02:29:40 +0530

    config firebaseConfig.js

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDpJZtEdsnOFORLxkux6mTv4mvV2KnQRP4",
+  authDomain: "quickdual-f5174.firebaseapp.com",
+  databaseURL: "https://quickdual-f5174-default-rtdb.firebaseio.com",
+  projectId: "quickdual-f5174",
+  storageBucket: "quickdual-f5174.firebasestorage.app",
+  messagingSenderId: "782418843732",
+  appId: "1:782418843732:web:1e4a9104f0a5a399b8b5c1",
+  measurementId: "G-D69NGL2GFG"
+};
