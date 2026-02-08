3365d4f5b880903e4e0a364edc290b85ba3ab03d
Aditya Gorane <168202112+AdiGo112@users.noreply.github.com>
2026-02-09 03:14:17 +0530

    database connect

--- src/config/postgresConfig.js
+++ src/config/postgresConfig.js
@@ -1,3 +1,10 @@
 import postgres from 'postgres';
+import dotenv from 'dotenv';
 
-export const sql = postgres(process.env.DATABASE_URL,  { ssl: 'verify-full' });
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
+export const sql = postgres(process.env.DATABASE_URL, { ssl: 'require' });
